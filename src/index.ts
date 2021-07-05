f1fac8273a290bfd4fd7bcce7a6fbb8d2739ed99
Ludwig Granstedt <ludwiggranstedt@gmail.com>
2021-07-05 19:49:35 +0200

    Bot up and working

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Client, MessageEmbed } from "discord.js";
+
+const { token, dinky } = require("../config.json"); // config file with your token
+
+const client = new Client();
+
+client.once("ready", () => {
+    console.log("ready");
+});
+
+client.on("guildCreate", (guild) => {
+    if (guild.member(dinky)) {
+        console.log(`Joined ${guild.name} and Dinky is here`);
+    }
+});
+
+client.on("message", (message) => {
+    if (message.content.toLocaleLowerCase().includes("komsi komsi dinky")) {
+        client.users.fetch(dinky).then((user) => {
+            let embed = new MessageEmbed()
+                .setColor(message.member?.roles.color?.color.toString(16) as string)
+                .setAuthor(message.author.tag, message.author.avatarURL() as string)
+                .setDescription(message.content)
+                .addField("\u200B", `[link](${message.url})`)
+                .setTimestamp();
+            user.send("You have been summoned!", embed);
+        });
+    }
+})
+
+client.login(token);
