9b63fcfe3a2b166eec5a4089a2494da74e6a8a4a
Ludwig Granstedt <ludwiggranstedt@gmail.com>
2021-07-05 20:29:32 +0200

    fixed silly stuff

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ client.on("guildCreate", (guild) => {
 });
 
 client.on("message", (message) => {
-    if (message.content.toLocaleLowerCase().includes("komsi komsi dinky\n[link](${message.url})")) {
+    if (message.content.toLocaleLowerCase().includes("komsi komsi dinky")) {
         client.users.fetch(dinky).then((user) => {
             let embed = new MessageEmbed()
                 .setColor(message.member?.roles.color?.color.toString(16) as string)
                 .setAuthor(message.author.tag, message.author.avatarURL() as string)
-                .setDescription(message.content)
+                .setDescription(`${message.content}\n[link](${message.url})`)
                 .setTimestamp();
             user.send("You have been summoned!", embed);
         });
